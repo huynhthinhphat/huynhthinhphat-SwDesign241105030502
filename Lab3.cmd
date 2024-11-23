3f76f3d2cb81b9c32519a16a12fab76548851476
Huỳnh Thịnh Phát <132771325+huynhthinhphat@users.noreply.github.com>
2024-11-23 12:13:41 +0700

    Update Lab3.cmd

--- Lab3.cmd
+++ Lab3.cmd
@@ -1,1 +1,5 @@
+Lab 3. Identify design elements
+1. Subsystem context diagrams
+  - Biểu đồ ngữ cảnh của hệ thống con BankSystem.
+    ![Diagram](https://www.planttext.com/api/plantuml/png/h59BQiCm4Dth5BDh2ZNP6sD2surtXPv0bST4H3yqurBJzcHPv4YvGYmxIULaMos6OMRUXuUE-yE6M4RkhH4Fy8gTTWZAEuxUW3AII4mLtkGmZipBl38OuLl0-DPhYBs3CAzNZ-9N_15fnnWxgL1SEsYUfTkz3yHepnGj1a-QLtBKK5jKkn8ktLEYy0bHGggDwtoqahLtIoNgFsZc3D5_3MVJcLHLtPpKzJ_AesFD6hEm9dnc8uDck8Xc6j5M1nBt6HT5lGYiX5P7L7mnJJ3f47ngAVB3wgSeO05iR31eHwEqE4mALxktYplZ6tHj-Yed0000__y30000)
 
